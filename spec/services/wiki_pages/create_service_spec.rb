e20e2a966155e459cbc1da3add711940c3b6182d
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2017-06-13 12:38:54 -0300

    Resolve conflicts in WikiPages services

--- spec/services/wiki_pages/create_service_spec.rb
+++ spec/services/wiki_pages/create_service_spec.rb
@@ -2,11 +2,7 @@ require 'spec_helper'
 
 describe WikiPages::CreateService, services: true do
   let(:project) { create(:empty_project) }
-<<<<<<< HEAD
-  let(:user)    { create(:user) }
-=======
   let(:user) { create(:user) }
->>>>>>> ce/9-3-stable
 
   let(:opts) do
     {
@@ -19,11 +15,7 @@ describe WikiPages::CreateService, services: true do
   subject(:service) { described_class.new(project, user, opts) }
 
   before do
-<<<<<<< HEAD
-    project.add_master(user)
-=======
     project.add_developer(user)
->>>>>>> ce/9-3-stable
   end
 
   describe '#execute' do
@@ -31,13 +23,7 @@ describe WikiPages::CreateService, services: true do
       page = service.execute
 
       expect(page).to be_valid
-<<<<<<< HEAD
-      expect(page).to have_attributes(title: opts[:title], content: opts[:content], format: opts[:format].to_sym)
-    end
 
-    it 'executes webhooks' do
-      expect(service).to receive(:execute_hooks).once.with(instance_of(WikiPage), 'create')
-=======
       expect(page.title).to eq(opts[:title])
       expect(page.content).to eq(opts[:content])
       expect(page.format).to eq(opts[:format].to_sym)
@@ -46,7 +32,6 @@ describe WikiPages::CreateService, services: true do
     it 'executes webhooks' do
       expect(service).to receive(:execute_hooks).once
         .with(instance_of(WikiPage), 'create')
->>>>>>> ce/9-3-stable
 
       service.execute
     end
--- spec/services/wiki_pages/destroy_service_spec.rb
+++ spec/services/wiki_pages/destroy_service_spec.rb
@@ -2,32 +2,19 @@ require 'spec_helper'
 
 describe WikiPages::DestroyService, services: true do
   let(:project) { create(:empty_project) }
-<<<<<<< HEAD
-  let(:user)    { create(:user) }
-  let(:page)    { create(:wiki_page) }
-=======
   let(:user) { create(:user) }
   let(:page) { create(:wiki_page) }
->>>>>>> ce/9-3-stable
 
   subject(:service) { described_class.new(project, user) }
 
   before do
-<<<<<<< HEAD
-    project.add_master(user)
-=======
     project.add_developer(user)
->>>>>>> ce/9-3-stable
   end
 
   describe '#execute' do
     it 'executes webhooks' do
-<<<<<<< HEAD
-      expect(service).to receive(:execute_hooks).once.with(instance_of(WikiPage), 'delete')
-=======
       expect(service).to receive(:execute_hooks).once
         .with(instance_of(WikiPage), 'delete')
->>>>>>> ce/9-3-stable
 
       service.execute(page)
     end
