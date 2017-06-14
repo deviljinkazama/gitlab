c5597df3dfbaf9f09ef45daaa3fe965595c04ae4
Clement Ho <ClemMakesApps@gmail.com>
2017-06-14 18:17:59 -0500

    Fix filtered_search_bundle conflict

--- app/assets/javascripts/filtered_search/filtered_search_bundle.js
+++ app/assets/javascripts/filtered_search/filtered_search_bundle.js
@@ -8,9 +8,6 @@ import './filtered_search_dropdown';
 import './filtered_search_manager';
 import './filtered_search_tokenizer';
 import './filtered_search_visual_tokens';
-<<<<<<< HEAD
 
 // EE-only
 import './filtered_search_token_keys_issues_ee';
-=======
->>>>>>> 0d9311624754fbc3e0b8f4a28be576e48783bf81
